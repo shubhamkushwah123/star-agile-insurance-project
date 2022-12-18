76a6be8ec0576c441dba73ba9ef7485d26006fdf
Shubham Singh Kushwah <shubhamkushwah123@gmail.com>
2022-12-18 09:43:49 +0530

    project solution updated

--- src/test/java/com/project/staragile/insureme/InsureMeApplicationTests.java
+++ src/test/java/com/project/staragile/insureme/InsureMeApplicationTests.java
@@ -1,5 +1,7 @@
 package com.project.staragile.insureme;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,21 @@ class InsureMeApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void testCreatePolicy() {
+		Policy policy = new Policy(1, "Shubham", "Individual" , 10000, "10-Sep-2021", "10-Sep-2022");
+		PolicyService pService = new PolicyService();
+		//Policy outputPolicy = pService.CreatePolicy();
+		assertEquals(policy.getPolicyId(), pService.generateDummyPolicy().getPolicyId());
+		
+	}
+	
+	@Test
+	void testSearchPolicy() {
+		PolicyService pService = new PolicyService();
+		assertEquals(null,pService.searchPolicy());
 	}
 
 	
